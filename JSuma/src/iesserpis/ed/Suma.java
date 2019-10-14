1c84ec12dc11f99eabd820d88fc2a1a1fd248cbc
luisvelasco1920 <luis.velasco@iesserpis.org>
2019-10-14 13:32:25 +0200

    Añadido JSuma mostrando while y for

---
+++ JSuma/src/iesserpis/ed/Suma.java
@@ -0,0 +1,19 @@
+package iesserpis.ed;
+
+import java.util.Scanner;
+
+public class Suma {
+
+	public static void main(String[] args) {
+		int contador = 1;
+		while (contador <= 5) {
+			System.out.println("contador = " + contador);
+			contador = contador + 1; //contador++
+		}
+		System.out.println("Fin del programa.");
+		
+//		for (int contador = 1; contador <=5; contador++)
+//			System.out.println("contador = " + contador);
+//		System.out.println("Fin del programa.");
+	}
+}
